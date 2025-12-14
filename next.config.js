df37779ff89c8f0e138d3769b8f0c586323822a6
Bhattacharya <aishee.bhattacharya@accenture.com>
2025-12-14 21:26:07 +0530

    Add static export config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
-    domains: ['img.youtube.com', 'i.ytimg.com'],
-  },
-  env: {
-    GEMINI_API_KEY_1: process.env.GEMINI_API_KEY_1,
-    GEMINI_API_KEY_2: process.env.GEMINI_API_KEY_2,
-    GEMINI_API_KEY_3: process.env.GEMINI_API_KEY_3,
+    unoptimized: true
   }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
